8e654b5385e5a76a8867c5e09acca373ff47be55
SanghoLee <tkdgh1321@ks.ac.kr>
2020-11-05 21:19:28 +0900

    Add project

---
+++ src/main/java/com/ksu/soccerserver/SoccerServerApplication.java
@@ -0,0 +1,13 @@
+package com.ksu.soccerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoccerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoccerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksu/soccerserver/SoccerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksu.soccerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoccerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

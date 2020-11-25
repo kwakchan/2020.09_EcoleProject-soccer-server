afadc2978756d4e80d6ef218476f1522e9c073cb
kiseok, Yang <rltjr219@gmail.com>
2020-11-25 17:28:09 +0900

    Resolve compileJava Error

--- src/main/java/com/ksu/soccerserver/board/BoardController.java
+++ src/main/java/com/ksu/soccerserver/board/BoardController.java
@@ -9,15 +9,12 @@ import com.ksu.soccerserver.board.dto.BoardDetailResponse;
 import com.ksu.soccerserver.team.TeamRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
-import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
-
-
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -26,24 +23,20 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 @RestController
 public class BoardController {
+
     private final AccountRepository accountRepository;
     private final BoardRepository boardRepository;
-    //private final ModelMapper modelMapper;
+    private final ModelMapper modelMapper;
     private final TeamRepository teamRepository;
 
 
     @PostMapping
     ResponseEntity<?> postBoard(@RequestBody BoardRequest boardRequest, @CurrentAccount Account currentAccount){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Account account = accountRepository.findById(currentAccount.getId())
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"NO_FOUND_ACCOUNT"));
 
         Board postBoard = boardRequest.toEntity(account);
-
         Board saveBoard = boardRepository.save(postBoard);
-
         BoardDetailResponse response = modelMapper.map(saveBoard, BoardDetailResponse.class);
 
         return new ResponseEntity<>(response, HttpStatus.CREATED);
@@ -51,23 +44,6 @@ public class BoardController {
 
     @GetMapping
     ResponseEntity<?> getBoardList() {
-        ModelMapper modelMapper = new ModelMapper();
-        //TypeMap typeMap = modelMapper.createTypeMap(boardRepository,BoardListRespnse.class)
-         //       .addMapping(boardRepository::getAccount, BoardListRespnse::setAccount);
-
-      /*  PropertyMap<Board, BoardListRespnse> bookMap = new PropertyMap<Board, BoardListRespnse>() {
-            protected void configure() {
-                map().setName(source.getAccount().getName());
-            }
-        };
-
-
-
-        modelMapper.addMappings(bookMap);
-*/
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
-
         List<BoardListResponse> boardListResponse = boardRepository.findAll()
                 .stream()
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class))
@@ -82,18 +58,7 @@ public class BoardController {
 
     @GetMapping("/{boardId}")
     ResponseEntity<?> getBoardDetail(@PathVariable Long boardId){
-        ModelMapper modelMapper = new ModelMapper();
-       /* PropertyMap<Board, BoardListRespnse> bookMap = new PropertyMap<Board, BoardListRespnse>() {
-            protected void configure() {
-                map().setName(source.getAccount().getName());
-            }
-        };
-
-        modelMapper.addMappings(bookMap);
-        */
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         Board findBoard = boardRepository.findById(boardId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"존재하지 않는 게시판입니다."));
-
 
         BoardDetailResponse response =modelMapper.map(findBoard, BoardDetailResponse.class);
 
@@ -104,8 +69,6 @@ public class BoardController {
 
     @GetMapping("/myBoard")
     ResponseEntity<?> getAccountsBoard(@CurrentAccount Account currentAccount) {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
         Account account = accountRepository.findById(currentAccount.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"NO_FOUND_ACCOUNT"));
 
         List<BoardListResponse> boardListResponses = boardRepository.findByAccount(account)
@@ -123,9 +86,6 @@ public class BoardController {
     //게시판 keyword포함 제목 검색
     @GetMapping("/search")
     ResponseEntity<?> getSearchBoard(@RequestParam(value = "keyword") String keyword){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         List<BoardListResponse> boardListResponses = boardRepository.findByTitleContaining(keyword)
                 .stream()
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class))
@@ -138,44 +98,24 @@ public class BoardController {
         return new ResponseEntity<>(boardListResponses, HttpStatus.OK);
     }
 
-    //boardType 게시판 출력
-    /*
-    @GetMapping("/boardType")
-    ResponseEntity<?> getFilteredBoard(@RequestParam(value = "keyword")String keyword){
-        List<Board> boards = boardRepository.findByBoardtype(keyword);
-        if(boards.isEmpty()){
-            return new ResponseEntity<>("게시글이 없습니다.", HttpStatus.NOT_FOUND);
-        }
-
-        return new ResponseEntity<>(boards, HttpStatus.OK);
-    }
-    */
-
-    //////////////////////////
-    //boardPagination
     @GetMapping("/page")
     ResponseEntity<?> getPaginationBoard(Pageable pageable){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Page<BoardListResponse> boardListResponses = boardRepository.findAll(pageable)
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class));
-
 
         if(boardListResponses.isEmpty()) {
             return new ResponseEntity<>("게시글이 없습니다", HttpStatus.NOT_FOUND);
         }
+
         return new ResponseEntity<>(boardListResponses, HttpStatus.OK);
     }
 
     @GetMapping("/myBoard/page")
     ResponseEntity<?> getPaginationAccountsBoard(@CurrentAccount Account currentAccount, Pageable pageable) {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Page<BoardListResponse> boardListResponses = boardRepository.findAllByAccount(accountRepository.findById(currentAccount.getId()).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"NOT ACCOUNT")), pageable)
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class));
-       if(boardListResponses.isEmpty()){
+
+        if(boardListResponses.isEmpty()){
             return new ResponseEntity<>(currentAccount.getName()+"님이 작성한 개시글이 없습니다.", HttpStatus.NOT_FOUND);
         }
 
@@ -184,11 +124,9 @@ public class BoardController {
 
     @GetMapping("/search/page")
     ResponseEntity<?> getPaginationSearchBoard(@RequestParam(value = "keyword") String keyword, Pageable pageable) {
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Page<BoardListResponse> boardListResponses = boardRepository.findAllByTitleContaining(keyword, pageable)
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class));
+
         if (boardListResponses.isEmpty()) {
             return new ResponseEntity<>(keyword + " 제목의 게시글이 없습니다.", HttpStatus.NOT_FOUND);
         }
@@ -198,9 +136,6 @@ public class BoardController {
 
     @GetMapping("/boardType/page")
     ResponseEntity<?> getPaginationFilteredBoard(@RequestParam(value = "keyword")String keyword, Pageable pageable){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Page<BoardListResponse> boardListResponses = boardRepository.findAllByBoardType(keyword, pageable)
                 .map(boardRepository -> modelMapper.map(boardRepository, BoardListResponse.class));
         if(boardListResponses.isEmpty()){
@@ -212,24 +147,20 @@ public class BoardController {
 
     @PutMapping("/{boardId}")
     ResponseEntity<?> putBoard(@PathVariable Long boardId, @RequestBody BoardRequest boardRequest,@CurrentAccount Account currentAccount){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Board findBoard = boardRepository.findById(boardId)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 게시판입니다."));
 
         if(!currentAccount.getId().equals(findBoard.getAccount().getId())){
             return new ResponseEntity<>("수정권한이 없습니다.", HttpStatus.BAD_REQUEST);
         }else {
-
-
             findBoard.setTitle(boardRequest.getTitle());
             findBoard.setContent(boardRequest.getContent());
             findBoard.setTime(LocalDateTime.now());
             findBoard.setBoardtype(boardRequest.getBoardType());
-
             boardRepository.save(findBoard);
+
             BoardDetailResponse response = modelMapper.map(findBoard, BoardDetailResponse.class);
+
             return new ResponseEntity<>(response, HttpStatus.OK);
         }
 
@@ -237,17 +168,16 @@ public class BoardController {
 
     @DeleteMapping("/{boardId}")
     ResponseEntity<?> deleteBoard(@PathVariable Long boardId, @CurrentAccount Account currentAccount){
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.LOOSE);
-
         Board board = boardRepository.findById(boardId).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "존재하지 않는 게시판입니다."));
+
         if(!currentAccount.getId().equals(board.getAccount().getId())){
             return new ResponseEntity<>("삭제권한이 없습니다.", HttpStatus.BAD_REQUEST);
-        }else {
+        }
+        else {
             boardRepository.delete(board);
             BoardDetailResponse response = modelMapper.map(board, BoardDetailResponse.class);
-            return new ResponseEntity(response, HttpStatus.OK);
+
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
     }
-
 }

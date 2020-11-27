79bfc0092e58b0302a4accc3c8275f8bd741c23f
Kiseok Yang <rltjr219@gmail.com>
2020-11-28 00:46:21 +0900

    Update BoardRepository.java

--- src/main/java/com/ksu/soccerserver/board/BoardRepository.java
+++ src/main/java/com/ksu/soccerserver/board/BoardRepository.java
@@ -12,13 +12,10 @@ public interface BoardRepository extends JpaRepository<Board, Long>{
 
     List<Board> findAllByBoardTypeAndTitleContaining(String boardType, String title);
 
-<<<<<<< HEAD
     List<Board> findAllByTitleContaining(String title);
 
-=======
     List<Board> findByTitleContainingAndBoardType(String title, String boardType);
   
->>>>>>> 982cad2a24175ef23bf24e948444ef59b0b928ea
     //pagination
     Page<Board> findAllByAccount(Account account, Pageable pageable);
 
